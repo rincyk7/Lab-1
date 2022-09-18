be10826d6107c79a2a7d23a010268274c3d0eb45
rincyk7 <113142186+rincyk7@users.noreply.github.com>
2022-09-18 09:25:21 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+//lib.js
+import { halfOf, multiply } from 'lib';
+console.log(halfOf(84));
+console.log(multiply(21, 2));
+
+
+//dosomething.js
+import doSomething from 'doSomething';
+doSomething();
+
+//Validator.js
+import { flag, touch } from 'validator';
+console.log(flag);
+touch();
+console.log(flag);
